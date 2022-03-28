f768004f23ea53840a941805e71097a79e69770d
justteen <48372964+justteen@users.noreply.github.com>
2022-03-28 07:20:55 +0700

    Add files via upload

---
+++ BuzzMusicBot/utils/channelplay.py
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2021-2022 by justteen@Github, < https://github.com/justteen >.
+#
+# This file is part of < https://github.com/justee/BuzzMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/justteen/BuzzMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+from BuzzMusicBot import app
+from BuzzMusicBot.utils.database import get_cmode
+
+
+async def get_channeplayCB(_, command, CallbackQuery):
+    if command == "c":
+        chat_id = await get_cmode(CallbackQuery.message.chat.id)
+        if chat_id is None:
+            try:
+                return await CallbackQuery.answer(
+                    _["setting_12"], show_alert=True
+                )
+            except:
+                return
+        try:
+            chat = await app.get_chat(chat_id)
+            channel = chat.title
+        except:
+            try:
+                return await CallbackQuery.answer(
+                    _["cplay_4"], show_alert=True
+                )
+            except:
+                return
+    else:
+        chat_id = CallbackQuery.message.chat.id
+        channel = None
+    return chat_id, channel
